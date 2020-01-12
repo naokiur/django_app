83b4376d52c63b8d51d92b65c7a47fc144161d09
naokiur <atomic.transfer.comp@gmail.com>
2020-01-12 21:37:35 +0900

    sample 7

--- ebdjango/quickstart/views.py
+++ ebdjango/quickstart/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Sample6 Hello, world. You're at the quickstart index.")
+	return HttpResponse("Sample7 Hello, world. You're at the quickstart index.")
